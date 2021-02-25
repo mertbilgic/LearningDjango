60888a474106dc64bf690453f2fb87ad087a1d58
Mert <bilgic.mert44@hotmail.com>
2021-02-25 13:18:22 +0300

    Simple Django REST framework App

---
+++ countries/currency_country/serializers.py
@@ -0,0 +1,8 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Country
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = ('country_name', 'local_currency')
